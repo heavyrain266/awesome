5269de1dedeb47a9f46b37aa67829b2413e66c6b
HeavyRain266 <ignacy423@gmail.com>
2022-06-02 22:45:05 +0200

    Auto install bling if not exists

--- useless/autostart.lua
+++ useless/autostart.lua
@@ -2,11 +2,29 @@
 
 local awful = require "awful"
 
+-- Check if file/dir exists
+function exists(path)
+    local ok = os.rename(path, path)
+
+    if ok == nil then
+        if ok == 17 then
+            return true
+        end
+        return false
+    end
+    return true
+end
+
+if not exists "./bling" then
+    os.execute "git submodule update --init"
+end
+
 local cmds = {
     "xset -b",
     "redshift -P -O 4600",
     "setxkbmap -option caps:hyper",
     "picom -b --experimental-backends",
+    "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
 }
 
 for cmd = 1, #cmds do
